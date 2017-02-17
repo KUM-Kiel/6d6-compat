bcd4c6c0347408b7698bad69fe0c1dcd5e6905c8
Lukas Joeressen <lukas@joeressen.net>
2017-02-17 14:19:52 +0100

    Version 1.2.0

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define KUM_6D6_COMPAT_VERSION "1.1.1"
-#define KUM_6D6_COMPAT_DATE    "2017-01-06"
+#define KUM_6D6_COMPAT_VERSION "1.2.0"
+#define KUM_6D6_COMPAT_DATE    "2017-02-17"
