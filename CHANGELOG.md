e9700121d0af5f5ddb883b270ff9939771ee3169
Lukas Joeressen <lukas@joeressen.net>
2021-01-19 15:55:09 +0100

    Version 1.6.3.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 All notable changes to this project will be documented in this file.
 
 ## [Unreleased]
+
+## [1.6.3] - 2021-01-19
 ### Added
 - New tool `6d6strip`.
 
--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define KUM_6D6_COMPAT_VERSION "1.6.2"
-#define KUM_6D6_COMPAT_DATE    "2020-08-28"
+#define KUM_6D6_COMPAT_VERSION "1.6.3"
+#define KUM_6D6_COMPAT_DATE    "2021-01-19"
