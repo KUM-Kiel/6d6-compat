64516d9c0a0295ca1068336b98b0c3424912dd23
Lukas Joeressen <lukas@joeressen.net>
2023-09-13 17:08:15 +0200

    Version 1.6.9.

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
 #pragma once
 
-#define KUM_6D6_COMPAT_VERSION "1.6.8"
-#define KUM_6D6_COMPAT_DATE    "2023-07-07"
+#define KUM_6D6_COMPAT_VERSION "1.6.9"
+#define KUM_6D6_COMPAT_DATE    "2023-09-13"
